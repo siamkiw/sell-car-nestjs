0e15a923d6925c38a8d0ad889c2627312c650de4
Siam.sae <siam.sae@AGC-C02HD145Q05P.local>
2022-04-25 01:48:08 +0700

    add auth guard and decorator interceptor currentUser in user route

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,13 @@
-import { Controller, Post, Body, Get, Param, Query, Delete, Patch, NotFoundException, Session } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, Query, Delete, Patch, NotFoundException, Session, UseGuards } from '@nestjs/common';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UserDto } from './dtos/user.dto';
 import { UsersService } from './users.service';
 import { AuthService } from './auth.service';
+import { CurrentUser } from './decorators/current-user.decorator';
+import { User } from './user.entity';
+import { AuthGuard } from '../guards/auth.guard';
 
 @Controller('auth')
 @Serialize(UserDto)
@@ -14,14 +17,15 @@ export class UsersController {
         private authService: AuthService
     ) { }
 
-    @Get('whoami')
-    whoAmI(@Session() session: any) {
-        return this.usersService.findOne(session.userId)
-    }
+    // @Get('whoami')
+    // whoAmI(@Session() session: any) {
+    //     return this.usersService.findOne(session.userId)
+    // }
 
-    @Post('/signout')
-    signOut(@Session() session: any) {
-        session.userId = null
+    @Get('/whoami')
+    @UseGuards(AuthGuard)
+    whoAmI(@CurrentUser() user: User) {
+        return user
     }
 
     @Post('/signup')
@@ -36,6 +40,11 @@ export class UsersController {
         const user = await this.authService.signin(body.email, body.password)
         session.userId = user.id
         return user
+    }
+
+    @Post('/signout')
+    signOut(@Session() session: any) {
+        session.userId = null
     }
 
     @Get('/:id')
